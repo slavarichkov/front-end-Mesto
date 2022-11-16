6bb17e7753570e2e0d3b44a9d63521f20e2fee91
Slava Richkov <v.v.richkov@yandex.ru>
2022-11-16 14:53:06 +0500

    style

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,30 +1,31 @@
 import { React } from 'react';
 import { Link } from 'react-router-dom';
 import logo from './../images/LogoMesto.svg';
-import { Route } from 'react-router-dom';
-// управлять отрисовкой страницы в зависимости от пути в строке - dataLink
+import { Route, Switch } from 'react-router-dom';
 
 function Header({ handleLoggedInStateChanged }) {
 
     return (
         <header className="header">
             <img className="header__logo" src={logo} alt="Логотип"></img>
-            <Route path="/sign-up">
-                <div className="header__text-container">
-                    <Link className="button-text" to="/sign-in">Войти</Link>
-                </div>
-            </Route>
-            <Route path="/sign-in">
-                <div className="header__text-container">
-                    <Link className="button-text" to="/sign-up">Регистрация</Link>
-                </div>
-            </Route>
-            <Route exact path="/">
-                <div className="header__text-container">
-                    <h2 className="button-text_out">{localStorage.getItem('email')}</h2>
-                    <button className="button-text_out" onClick={() => handleLoggedInStateChanged(false)}>Выйти</button>
-                </div>
-            </Route>
+            <Switch>
+                <Route path="/sign-up">
+                    <div className="header__text-container">
+                        <Link className="button-text" to="/sign-in">Войти</Link>
+                    </div>
+                </Route>
+                <Route path="/sign-in">
+                    <div className="header__text-container">
+                        <Link className="button-text" to="/sign-up">Регистрация</Link>
+                    </div>
+                </Route>
+                <Route exact path="/">
+                    <div className="header__text-container">
+                        <h2 className="button-text_out">{localStorage.getItem('email')}</h2>
+                        <button className="button-text_out" onClick={() => handleLoggedInStateChanged(false)}>Выйти</button>
+                    </div>
+                </Route>
+            </Switch>
         </header >
     )
 }
